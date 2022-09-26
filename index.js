a7bd683b5097407c91fd4e5ff3596d2d2eb61559
Tom Va <tomtongva@gmail.com>
2022-09-25 20:09:49 -0400

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express") //import express library
+const app = express() //app is an instance of express
+const port = 3000 //port number for application
+
+app.listen(port, () => { //declare a web server
+   		console.log(`Listening at http://localhost:${port}`) //careful with slanted single quote
+});
+
+app.get('/', (req, res) => { //can declare get our put route, first param is the route, second param is the function that is executed
+    res.send("Hello world");
+});
